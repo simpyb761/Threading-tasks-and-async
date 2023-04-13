236f5ece5379eca34a772002d361c64c15d915e5
simpyb761 <simpyb761@gmail.com>
2023-04-13 16:24:32 -0400

    Completed Programs

---
+++ Tasks/Tasks/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace threadRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read, Set Go...");
+            int t1Location = 0;
+            int t2Location = 0;
+            int t3Location = 0;
+            int t4Location = 0;
+            int t5Location = 0;
+
+            //Creating Threads
+            Task runnerOne = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Lux";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t1Location);
+                }
+
+            });
+            Task runnerTwo = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Ahri";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t2Location);
+
+                }
+            });
+            Task runnerThree = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Leblanc";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t3Location);
+                }
+            });
+            Task runnerFour = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Neeko";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t4Location);
+                }
+            });
+            Task runnerFive = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Jinx";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t5Location);
+                }
+            });
+            
+
+            //Executing the methods
+            Task.WaitAny(runnerOne, runnerTwo, runnerThree, runnerFour, runnerFive);
+            Console.WriteLine("Race has ended");
+        }
+        static void MoveIt(ref int location)
+        {
+            location++;
+            Console.WriteLine($"{Thread.CurrentThread.Name} location={location}");
+            if (location >= 100)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} is the winner");
+                return;
+            }
+        }
+
+    }
+}
---
+++ Threads/Threads/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using static System.Console;
+namespace threadRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read, Set Go...");
+            int t1Location = 0;
+            int t2Location = 0;
+            int t3Location = 0;
+            int t4Location = 0;
+            int t5Location = 0;
+
+            //Creating Threads
+            Thread t1 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Lux";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location<100 && t4Location<100 && t5Location <100)
+                        MoveIt(ref t1Location);
+                }
+
+            });
+            Thread t2 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Ahri";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t2Location);
+
+                }
+            });
+            Thread t3 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Leblanc";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t3Location);
+                }
+            });
+            Thread t4 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Neeko";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t4Location);
+                }
+            });
+            Thread t5 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Jinx";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t5Location);
+                }
+            });
+
+            //Executing the methods
+            t2.Priority = ThreadPriority.Highest;
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            t5.Start();
+            Console.WriteLine("Race has ended");
+        }
+        static void MoveIt(ref int location)
+        {
+            location++;
+            Console.WriteLine($"{Thread.CurrentThread.Name} location={location}");
+            if (location >= 100)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} is the winner");
+                return;
+            }
+        }
+
+    }
+}
